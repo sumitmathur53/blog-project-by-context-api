ef8bdfa8ca5e6427e7fc2cb4fe08f17ab9aa7546
sumit kumar <sumitmathur170@gmail.com>
2023-07-06 23:15:47 +0530

    context api use blog project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import Header from "./components/Header";
+import Blog from "./components/Blog";
+import Pagination from "./components/Pagination";
+import { useContext, useEffect } from "react";
+import { AppContext } from "./context/AppContext";
+import "./App.css"
+
+export default function App() {
+  const {fetchBlogPosts} = useContext(AppContext);
+
+  useEffect(() => {
+    fetchBlogPosts();
+  },[]);
+
+  return (
+    <div className="w-full h-full flex flex-col gap-y-1 justify-center items-center">
+      <Header />
+
+      <Blog/>
+      <Pagination />
+    </div>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='w-full border shadow-md py-4  fixed top-0 bg-white'>
+      <header className='text-center'>
+        <h1 className='text-xl font-bold '>CODEHELP BLOGS</h1>
+      </header>
+    </div>
+  )
+}
